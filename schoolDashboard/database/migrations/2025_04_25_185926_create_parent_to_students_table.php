2572923953bf8f59c2f7ecc4ee74ed1cab6fdce3
vernonthedev <techjaja2@gmail.com>
2025-04-25 22:18:18 +0300

    [config] setup the project migrations.

---
+++ schoolDashboard/database/migrations/2025_04_25_185926_create_parent_to_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parent_to_students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('gender');
+            $table->string('occupation')->nullable();
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parent_to_students');
+    }
+};
---
+++ schoolDashboard/database/migrations/2025_04_25_190022_create_trips_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('sourceRoute');
+            $table->string('destinationRoute');
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->date('dateOfTrip');
+            $table->enum('tripStatus',['scheduled','ongoing','completed','cancelled'])->default('scheduled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
