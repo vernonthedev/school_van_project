e327801d4c61818ec597c99a2b234173712c9588
vernonthedev <techjaja2@gmail.com>
2025-04-25 23:35:14 +0300

    [config] added a property for making sure the models can be seeded.

--- schoolDashboard/app/Models/ParentToStudent.php
+++ schoolDashboard/app/Models/ParentToStudent.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ParentToStudent extends Model
 {
+    use HasFactory;
     // allowed columns
     protected $fillable = ['name','longitude','latitude','phoneNumber','gender','occupation','address'];
 
--- schoolDashboard/app/Models/Trip.php
+++ schoolDashboard/app/Models/Trip.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Trip extends Model
 {
+    use HasFactory;
     //allowed columns
     protected $fillable = ['sourceRoute','destinationRoute','startTime','endTime','dateOfTrip','tripStatus'];
 
