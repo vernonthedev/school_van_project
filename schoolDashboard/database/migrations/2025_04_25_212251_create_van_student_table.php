103425a8854a2de509b151c2a4695ad66a0cdd96
vernonthedev <techjaja2@gmail.com>
2025-04-26 00:26:14 +0300

    [fix] reordered the trips migration to be the last one to run.

--- schoolDashboard/database/migrations/2025_04_25_212251_create_van_student_table.php
+++ schoolDashboard/database/migrations/2025_04_25_212251_create_van_student_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // this is a pivot table linking the vans to students in a many to many relationship
         Schema::create('van_student', function (Blueprint $table) {
             $table->id();
             $table->foreignId('van_id')->constrained()->onDelete('cascade');
             $table->foreignId('student_id')->constrained()->onDelete('cascade');
             $table->timestamps();
-            // Ensuring unique combinations
             $table->unique(['van_id', 'student_id']);
-            $table->timestamps();
         });
     }
 
