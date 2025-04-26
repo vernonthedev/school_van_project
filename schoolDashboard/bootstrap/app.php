026a53f2b48de669c45a481a51f1907495dc6e58
vernonthedev <techjaja2@gmail.com>
2025-04-26 03:42:01 +0300

    [stage] api routes stage one works.

--- schoolDashboard/bootstrap/app.php
+++ schoolDashboard/bootstrap/app.php
@@ -14,10 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'ability' => CheckAbilities::class,
-            'abilities' => CheckForAnyAbility::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
