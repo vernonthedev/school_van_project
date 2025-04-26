c70f4758e88d422ec641ea5efb5765abc71e281d
vernonthedev <techjaja2@gmail.com>
2025-04-26 03:29:18 +0300

    [config] removed unnecessary middleware and abilities to be implemented in next staging.

--- schoolDashboard/routes/api.php
+++ schoolDashboard/routes/api.php
@@ -12,7 +12,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 // === ROUTES PER ROLE ===
 
 // ADMIN - can access everything
-Route::middleware(['auth:sanctum', 'ability:*'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/students', App\Http\Controllers\API\StudentController::class);
     Route::apiResource('/vans', App\Http\Controllers\API\VanController::class);
     Route::apiResource('/drivers', App\Http\Controllers\API\DriverController::class);
@@ -23,7 +23,7 @@ Route::middleware(['auth:sanctum', 'ability:*'])->group(function () {
 
 
 // PARENT ROUTES
-Route::middleware(['auth:sanctum', 'ability:parent:view-self,parent:update-self,student:view-own,trip:*,van:view,driver:view'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/parent_to_students', App\Http\Controllers\API\ParentToStudentController::class);
     Route::apiResource('/students', App\Http\Controllers\API\StudentController::class)->only(['index', 'show']);
     Route::apiResource('/vans', App\Http\Controllers\API\VanController::class)->only(['index', 'show']);
@@ -33,7 +33,7 @@ Route::middleware(['auth:sanctum', 'ability:parent:view-self,parent:update-self,
 
 
 // DRIVER ROUTES
-Route::middleware(['auth:sanctum', 'ability:trip:*,van:view,operator:view'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/trips', App\Http\Controllers\API\TripController::class);
     Route::apiResource('/vans', App\Http\Controllers\API\VanController::class)->only(['index', 'show']);
     Route::apiResource('/operators', App\Http\Controllers\API\OperatorController::class)->only(['index', 'show']);
@@ -41,8 +41,11 @@ Route::middleware(['auth:sanctum', 'ability:trip:*,van:view,operator:view'])->gr
 
 
 // OPERATOR ROUTES
-Route::middleware(['auth:sanctum', 'ability:trip:*,van:view,driver:view'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/trips', App\Http\Controllers\API\TripController::class);
     Route::apiResource('/vans', App\Http\Controllers\API\VanController::class)->only(['index', 'show']);
     Route::apiResource('/drivers', App\Http\Controllers\API\DriverController::class)->only(['index', 'show']);
 });
+
+
+//todo: implement token abilities
